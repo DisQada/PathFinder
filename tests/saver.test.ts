2b5246edb6a546cc786c8d5ae4d764900d46ba55
The Alpha <contact@nabilalsaiad.com>
2023-04-14 19:03:18 +0800

    test(helpers/saver): moved tests to respective files to match src files

--- tests/saver.test.ts
+++ tests/saver.test.ts
@@ -1,5 +1,25 @@
 import { expect, test } from "vitest";
-import { storeFilePathsInFolders } from "../src/saver";
+import { getFilePathsInFolder, storeFilePathsInFolders } from "../src/saver";
+
+test("getFilePathsInFolder function", async () => {
+	const folderPaths = ["src", "test"];
+
+	for (let i = 0; i < folderPaths.length; i++) {
+		const paths = getFilePathsInFolder(folderPaths[i]);
+		expect(paths).to.be.an("array");
+	}
+
+	for (let i = 0; i < folderPaths.length; i++) {
+		const paths = getFilePathsInFolder(folderPaths[i], true);
+		expect(paths).to.be.an("array");
+	}
+
+	try {
+		getFilePathsInFolder(__filename);
+	} catch (err) {
+		expect(err).toBeInstanceOf(Error);
+	}
+});
 
 test("filePath class initialising", async () => {
 	expect(await storeFilePathsInFolders()).not.toBeInstanceOf(Error);
---
+++ tests/utilities.test.ts
@@ -0,0 +1,13 @@
+import { expect, test } from "vitest";
+import { workspaceFolders } from "../src/utilities";
+
+const folderPaths = await workspaceFolders();
+
+test("getWorkspaceFolders function", async () => {
+	expect(folderPaths).to.be.an("array");
+	if (folderPaths.length === 2) {
+		expect(folderPaths).toEqual(["src", "tests"]);
+	} else {
+		expect(folderPaths).toEqual(["dist", "src", "tests"]);
+	}
+});
