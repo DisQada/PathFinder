e63cc6d61463b189bb3418687050a11ab7ade343
The Alpha <contact@nabilalsaiad.com>
2023-07-19 21:46:53 +0800

    feat(finder): rewrite finder functions and move them to separate file
    Finding functions were moved to a new file and completely rewritten, the functions now take an
    options interface for powerful filtering
    
    BREAKING CHANGE: Renamed functions (aFilePath => findPath) and (allFilePaths => findPaths), the
    functions now take an interface as parameter

--- src/finder.ts
+++ src/finder.ts
@@ -1,8 +1,45 @@
 import { getPaths } from "./storage";
 import { FilePath } from "./types/filePath";
 
-export function aFilePath(fileName: string): FilePath | FilePath[] | undefined {
-    const paths = getPaths();
-    const values = paths.filter((fp) => fp.name === fileName);
-    return values.length === 1 ? values[0] : values;
+export interface filterOptions {
+    name?: string | RegExp;
+    extension?: string | RegExp;
+    folder?: string | RegExp;
+    root?: string | RegExp;
+}
+
+export function findPaths(options?: filterOptions): FilePath[] {
+    function test(path: FilePath, propertyName: string) {
+        // @ts-expect-error
+        const property = options?.[propertyName] || undefined;
+        if (!property) {
+            return true;
+        }
+
+        if (typeof property === "string") {
+            // @ts-expect-error
+            return property === path[propertyName];
+        } else {
+            // @ts-expect-error
+            return property.test(path[propertyName]);
+        }
+    }
+    const filePaths = getPaths();
+
+    if (!options) {
+        return filePaths;
+    }
+
+    return filePaths.filter((path) => {
+        return (
+            test(path, "name") &&
+            test(path, "extension") &&
+            test(path, "folder") &&
+            test(path, "root")
+        );
+    });
+}
+
+export function findPath(options: filterOptions): FilePath | undefined {
+    return findPaths(options)[0];
 }
