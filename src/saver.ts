a4dfec3bb6d8ffa17e42c0a4ae198ebb113f6669
The Alpha <contact@nabilalsaiad.com>
2023-07-20 08:15:40 +0800

    refactor(readfolderpaths): move readFolderPaths function
    Moved the readFolderPaths function from saver.ts to utilities.ts

--- src/saver.ts
+++ src/saver.ts
@@ -1,34 +1,6 @@
-import { readdir, stat } from "fs/promises";
-import { sep } from "path";
 import { SearchOptions } from "./helper/interfaces";
 import { storePaths } from "./storage";
-import { readWorkspaceFolderNames } from "./utilities";
-
-async function readFolderPaths(
-    folderPath: string,
-    options: SearchOptions
-): Promise<string[]> {
-    const stats = await stat(folderPath);
-    if (!stats.isDirectory()) {
-        throw new Error("Folder path is invalid: " + folderPath);
-    }
-
-    const allFiles: string[] = [];
-
-    for (const name of await readdir(folderPath)) {
-        const fullPath = folderPath + sep + name;
-        const stats = await stat(fullPath);
-
-        if (stats.isFile()) {
-            allFiles.push(fullPath);
-        } else if (options.deepSearch && stats.isDirectory()) {
-            const deepPaths = await readFolderPaths(fullPath, options);
-            allFiles.push(...deepPaths);
-        }
-    }
-
-    return allFiles;
-}
+import { readFolderPaths, readWorkspaceFolderNames } from "./utilities";
 
 export async function storeFolderPaths(
     folderPaths?: string[],
--- src/utilities.ts
+++ src/utilities.ts
@@ -1,4 +1,6 @@
-import { readdir } from "fs/promises";
+import { readdir, stat } from "fs/promises";
+import { sep } from "path";
+import { SearchOptions } from "./helper/interfaces";
 
 export async function readWorkspaceFolderNames(): Promise<string[]> {
     const invalidNameRegExp = /[._]/;
@@ -13,4 +15,30 @@ export async function readWorkspaceFolderNames(): Promise<string[]> {
     );
 
     return validFolderNames.map((folderName) => folderName.name);
+}
+
+export async function readFolderPaths(
+    folderPath: string,
+    options: SearchOptions
+): Promise<string[]> {
+    const stats = await stat(folderPath);
+    if (!stats.isDirectory()) {
+        throw new Error("Folder path is invalid: " + folderPath);
+    }
+
+    const allFiles: string[] = [];
+
+    for (const name of await readdir(folderPath)) {
+        const fullPath = folderPath + sep + name;
+        const stats = await stat(fullPath);
+
+        if (stats.isFile()) {
+            allFiles.push(fullPath);
+        } else if (options.deepSearch && stats.isDirectory()) {
+            const deepPaths = await readFolderPaths(fullPath, options);
+            allFiles.push(...deepPaths);
+        }
+    }
+
+    return allFiles;
 }
